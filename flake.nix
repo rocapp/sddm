407eefbffcc084a7526745e231d6e82547333e5b
Robbie Capps <rocapp@gmail.com>
2026-07-10 12:36:01 -0700

    added a flake.nix, scripts/build.sh (incorporates all build steps for convenience), bugfix for src/daemon/xorgdisplayserver.cpp (needed char wrapping for qlatin1char); now builds successfully using flake.nix (nix develop)

---
+++ flake.nix
@@ -0,0 +1,101 @@
+{
+  description = "SDDM — Simple Desktop Display Manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          sddm = pkgs.stdenv.mkDerivation {
+            pname = "sddm";
+            version = "0.19.0";
+
+            src = self;
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+              kdePackages.extra-cmake-modules
+              pkg-config
+              qt5.wrapQtAppsHook
+            ];
+
+            buildInputs = with pkgs; [
+              qt5.qtbase
+              qt5.qtdeclarative
+              qt5.qttools
+              pam
+              libxcb
+              systemd
+            ];
+
+            cmakeFlags = [
+              "-DBUILD_MAN_PAGES=OFF"
+              "-DENABLE_JOURNALD=ON"
+              "-DENABLE_PAM=ON"
+              "-DUID_MIN=1000"
+              "-DUID_MAX=65000"
+            ];
+
+            dontWrapQtApps = true;
+
+            doCheck = true;
+            checkPhase = ''
+              runHook preCheck
+              ctest --output-on-failure
+              runHook postCheck
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Simple Desktop Display Manager";
+              homepage = "https://github.com/sddm/sddm";
+              license = licenses.gpl2Plus;
+              platforms = platforms.linux;
+              maintainers = [ ];
+            };
+          };
+        in
+        {
+          default = sddm;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+
+            packages = with pkgs; [
+              ninja
+              gdb
+            ];
+
+            shellHook = ''
+              echo "SDDM dev shell — run 'nix develop' to enter"
+              echo "  ./scripts/build.sh — execute script to build SDDM."
+            '';
+          };
+        }
+      );
+
+      checks = forAllSystems (system: {
+        sddm = self.packages.${system}.default;
+      });
+    };
+}
